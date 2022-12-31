543e76cf13e184e11129c4e1cf80ae4022fc2d23
Jihyun Yu <yjh0502@gmail.com>
2022-12-31 21:59:18 +0900

    generate_gcode

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,10 @@ struct Opt {
     /// inject
     #[argh(switch)]
     inject: bool,
+
+    /// gcode
+    #[argh(option)]
+    gcode_filename: Option<String>,
 }
 
 // RLE, over Z axis,
@@ -119,15 +123,14 @@ impl MonotonicVoxel {
 
 #[derive(Default)]
 struct Model {
-    vertices: Vec<[i32; 3]>,
+    vertices: indexmap::IndexSet<[i32; 3]>,
     faces: Vec<[usize; 4]>,
 }
 
 impl Model {
     fn add_vert(&mut self, coord: [i32; 3]) -> usize {
-        let len = self.vertices.len();
-        self.vertices.push(coord);
-        return len;
+        let (idx, _) = self.vertices.insert_full(coord);
+        idx
     }
 
     fn add_face(&mut self, coord: [i32; 3], dir: [i32; 3]) {
@@ -166,14 +169,20 @@ impl Model {
         self.add_face(coord, [0, -1, -1]);
     }
 
-    fn serialize(&self, path: &str) -> Result<()> {
+    fn serialize(&self, path: &str, scale: f32) -> Result<()> {
         use std::io::Write;
 
         let w = File::create(path)?;
         let mut w = std::io::BufWriter::new(w);
 
         for [x, y, z] in &self.vertices {
-            write!(&mut w, "v {} {} {}\n", x, y, z)?;
+            write!(
+                &mut w,
+                "v {:.2} {:.2} {:.2}\n",
+                *x as f32 * scale,
+                *y as f32 * scale,
+                *z as f32 * scale
+            )?;
         }
         for [i0, i1, i2, i3] in &self.faces {
             write!(&mut w, "f {} {} {} {}\n", i0 + 1, i1 + 1, i2 + 1, i3 + 1)?;
@@ -273,7 +282,7 @@ fn generate_frames_constz() {
 
         let model = mv.to_model();
         let filename = format!("constz/test_{:03}.obj", idx);
-        model.serialize(&filename).unwrap();
+        model.serialize(&filename, 1f32).unwrap();
         idx += 1;
     }
 }
@@ -281,6 +290,10 @@ fn generate_frames_constz() {
 impl MonotonicVoxel {
     fn inject_at(&mut self, zlow: i32, zhigh: i32, pos0: [i32; 3], mut n: usize) {
         use std::collections::BinaryHeap;
+
+        if n == 0 {
+            return;
+        }
 
         #[derive(Clone, Copy, Ord, PartialEq, Eq, Debug)]
         struct HeapItem {
@@ -376,7 +389,7 @@ fn generate_inject() {
     }
 
     let model = mv.to_model();
-    model.serialize("inject.obj").unwrap();
+    model.serialize("inject.obj", 1f32).unwrap();
 }
 
 fn generate_frames() {
@@ -396,13 +409,121 @@ fn generate_frames() {
                         eprintln!("render={:?}", (x, y, z));
                         let model = mv.to_model();
                         let filename = format!("constblock/test_{:03}.obj", idx);
-                        model.serialize(&filename).unwrap();
+                        model.serialize(&filename, 1f32).unwrap();
                         idx += 1;
                     }
                 }
             }
         }
     }
+}
+
+fn generate_gcode(filename: &str) {
+    use nalgebra::Vector3;
+    use nom_gcode::{GCodeLine::GCode, Mnemonic};
+
+    // unit: 0.02mm, layer thickness: 0.2mm, nozzle size: 0.4mm
+    // 20mm
+    const UNIT: f32 = 0.04f32;
+
+    let mut mv = MonotonicVoxel::new();
+
+    let gcode = std::fs::read_to_string(filename).unwrap();
+
+    fn to_intpos(pos: [f32; 3]) -> [i32; 3] {
+        return [
+            (pos[0] / UNIT).round() as i32,
+            (pos[1] / UNIT).round() as i32,
+            (pos[2] / UNIT).round() as i32,
+        ];
+    }
+
+    let mut pos = Vector3::default();
+    let mut e = 0f32;
+    for line in gcode.lines() {
+        if let (_, Some(GCode(code))) = nom_gcode::parse_gcode(&line).unwrap() {
+            eprintln!("{}", line);
+            if code.mnemonic != Mnemonic::General {
+                continue;
+            }
+            if code.major == 0 {
+                for (letter, value) in code.arguments() {
+                    let letter = *letter;
+                    let v = value.unwrap();
+                    if letter == 'X' {
+                        pos[0] = v;
+                    }
+                    if letter == 'Y' {
+                        pos[1] = v;
+                    }
+                    if letter == 'Z' {
+                        pos[2] = v;
+                    }
+                }
+            } else if code.major == 1 {
+                let mut dst = pos;
+                let mut dst_e = e;
+                for (letter, value) in code.arguments() {
+                    let letter = *letter;
+                    let v = value.unwrap();
+                    if letter == 'X' {
+                        dst[0] = v;
+                    }
+                    if letter == 'Y' {
+                        dst[1] = v;
+                    }
+                    if letter == 'Z' {
+                        dst[2] = v;
+                    }
+                    if letter == 'E' {
+                        dst_e = v;
+                    }
+                }
+                if dst_e <= e {
+                    pos = dst;
+                    continue;
+                }
+
+                let dir = (dst - pos).normalize();
+                let len = (dst - pos).magnitude();
+
+                let total_blocks = (dst_e - e) * 25000f32;
+                let mut blocks = total_blocks as usize;
+                let step_size = 0.1;
+                let blocks_per_step = (total_blocks * step_size / len) as usize;
+
+                eprintln!(
+                    "{:?} -> {:?}, len={}, e={:?}, blocks={}",
+                    pos,
+                    dst,
+                    len,
+                    dst_e - e,
+                    total_blocks
+                );
+
+                let mut cursor = pos;
+                while (cursor - dst).magnitude() > step_size {
+                    let next = cursor + dir * step_size;
+                    let next_pos = to_intpos([next[0], next[1], next[2]]);
+                    let z = next_pos[2];
+                    mv.inject_at(z - 20, z, next_pos, blocks_per_step);
+                    cursor = next;
+                    blocks -= blocks_per_step;
+                }
+                {
+                    let next_pos = to_intpos([dst[0], dst[1], dst[2]]);
+                    let z = next_pos[2];
+                    mv.inject_at(z - 20, z, next_pos, blocks);
+                }
+
+                pos = dst;
+                e = dst_e;
+            }
+        }
+    }
+
+    let model = mv.to_model();
+    model.serialize("gcode.obj", UNIT).unwrap();
 }
 
 fn main() {
@@ -414,6 +535,8 @@ fn main() {
         generate_frames();
     } else if opt.inject {
         generate_inject();
+    } else if let Some(filename) = opt.gcode_filename {
+        generate_gcode(&filename);
     } else {
         let model = if opt.bruteforce {
             generate_brute_force()
@@ -423,6 +546,6 @@ fn main() {
             generate_face_only()
         };
 
-        model.serialize("test.obj").unwrap();
+        model.serialize("test.obj", 1f32).unwrap();
     }
 }
